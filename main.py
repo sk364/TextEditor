cdeae6d61788f825c401aa4b22006e8dfbf07a49
Sachin <skad5455@gmail.com>
2016-01-21 09:14:48 +0530

    added save and exit file options in file menu

--- main.py
+++ main.py
@@ -14,7 +14,10 @@ class Editor(QtGui.QMainWindow):
     	def initUI(self):
 		self.textEdit = QtGui.QTextEdit()
 		self.setCentralWidget(self.textEdit)
-		self.statusBar()
+		#self.statusBar()
+
+		self.cur_file = ''
+		self.is_saved = False
 
 		newFile = QtGui.QAction(QtGui.QIcon('new.png'), 'New', self)
 		newFile.setShortcut('Ctrl+N')
@@ -26,46 +29,94 @@ class Editor(QtGui.QMainWindow):
 		openFile.setStatusTip('Open new File')
 		openFile.triggered.connect(self.open_file)
 
+		saveAsFile = QtGui.QAction(QtGui.QIcon('saveAs.png'),'Save As',self)
+		saveAsFile.setStatusTip('Save As new or existing file')
+		saveAsFile.triggered.connect(self.saveAs_file)
+
 		saveFile = QtGui.QAction(QtGui.QIcon('save.png'),'Save',self)
-		saveFile.setShortcut('Ctrl+S')
-		saveFile.setStatusTip('Save new File')
-		saveFile.triggered.connect(self.save_file)
+                saveFile.setShortcut('Ctrl+S')
+                saveFile.setStatusTip('Save new File')
+                saveFile.triggered.connect(self.save_file)
+
+		exitFile = QtGui.QAction(QtGui.QIcon('exit.png'),'Exit',self)
+                exitFile.setShortcut('Ctrl+W')
+                exitFile.setStatusTip('Exit File')
+                exitFile.triggered.connect(self.exit_file)
+
+		self.textEdit.textChanged.connect(self.text_changes)
 
 		menubar = self.menuBar()
 		fileMenu = menubar.addMenu('&File')
 		fileMenu.addAction(newFile)
 		fileMenu.addAction(openFile)       
-		fileMenu.addAction(saveFile)    
-	    
-		self.setGeometry(300, 300, 350, 300)
+		fileMenu.addAction(saveFile)
+		fileMenu.addAction(saveAsFile)
+		fileMenu.addAction(exitFile)
+
+		self.setGeometry(100, 100, 800, 600)
 		self.setWindowTitle('Editor')
 		self.show()
 
 		
+	def text_changes(self):
+		self.setWindowTitle('Editor*')
+		is_saved = False
+
 	def new_file(self):
 		self.textEdit.clear()
 
 
     	def open_file(self):
-	
-        	fname = QtGui.QFileDialog.getOpenFileName(self, 'Open file', '/home')
-        
+        	fname = QtGui.QFileDialog.getOpenFileName(self, 'Open file', '/home')        
+
        		f = open(fname, 'r')
         
         	if f:        
+			self.cur_file = fname
             		data = f.read()
  	           	self.textEdit.setText(data) 
  	
-	def save_file(self):
-		fname = QtGui.QFileDialog.getSaveFileName(self, 'Save file', '/home', selectedFilter='*.txt')
+	def saveAs_file(self):
+		fname = QtGui.QFileDialog.getSaveFileName(self, 'Save As file', '/home', selectedFilter='*.txt')
 
 		text = self.textEdit.toPlainText()
 
 		if text:
-			f = open(fname,'w')	
+			f = open(fname,'w')
 			f.write(text)
+			self.is_saved = True
+			self.cur_file = fname
+			self.setWindowTitle('Editor')
 			f.close()
 	
+	def save_file(self):
+		text = self.textEdit.toPlainText()
+
+		if self.cur_file:	fname = open(self.cur_file, 'w')
+		elif text:
+			self.saveAs_file()
+			return
+		else:	return
+		
+		if fname:
+			fname.write(text)
+			self.is_saved = True
+			self.setWindowTitle('Editor')
+			fname.close()
+
+
+	def exit_file(self):
+		text = self.textEdit.toPlainText()
+
+		if self.is_saved or not text:	sys.exit()
+
+		elif self.cur_file:
+			# TODO show message for quitting or saving
+			self.save_file()
+		else:
+			self.saveAs_file()
+		
+			
         
 def main():
     
